33048003c30d2a063d3b4dbc71ba6883d54aa3d9
Matt Newville <newville@cars.uchicago.edu>
2011-03-09 20:48:56 -0600

    More changes... progress being made

---
+++ fortran/Autoconf_work/config.h
@@ -0,0 +1,23 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "xdilib"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "newville@cars.uchicago.edu"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "XDI Library"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "XDI Library 0.1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "xdi-library"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1.0"
+
+/* Version number of package */
+#define VERSION "0.1.0"
