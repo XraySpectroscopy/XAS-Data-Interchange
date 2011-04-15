08d09562c96355442aef22e027569fd04bc92076
Matt Newville <newville@cars.uchicago.edu>
2011-04-14 20:04:13 -0500

    moved some example (candidate) XDI files to data folder

--- fortran/xdi_example1.f
+++ fortran/xdi_example1.f
@@ -17,7 +17,8 @@
        double precision dat_it(max_pts), dat_if(max_pts)
        double precision dat_ir(max_pts)
 
-       fname = 'test.xdi'
+       fname = '../data/test_01.xdi'
+
        call read_xdi(fname,  max_attr, max_pts, max_comments,
      $      attr_names, attr_values, version, labels, comments,  
      $      npts, energy, dat_i0, dat_it, dat_if, dat_ir)
