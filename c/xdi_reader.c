ace43a125d931f7263d3c5b134fe6274c53d4b41
Matt Newville <newville@cars.uchicago.edu>
2012-03-07 20:29:58 -0600

    read arrays data... properly if ugly

--- c/xdi_reader.c
+++ c/xdi_reader.c
@@ -13,7 +13,7 @@
 #define MAX_LINES 16384      /* Max number of lines */
 #define MAX_WORDS 128
 /* Read function */
-int readlines(char *filename, char **lines); 
+int readlines(char *filename, char **lines);
 /* Output functions */
 void show_syntax(void);
 
@@ -23,7 +23,7 @@ void show_syntax(void);
 void show_syntax(void) {
   /* show command line syntax */
   printf("\nSyntax: xdi_reader filename\n");
-} 
+}
 /*-------------------------------------------------------*/
 
 /*-------------------------------------------------------*/
@@ -33,23 +33,19 @@ int readlines(char *filename, char **textlines) {
       as char *text[MAX] */
 
   FILE *inpFile;
-  char  thisline[MAX_LINE_LENGTH]; 
+  char  thisline[MAX_LINE_LENGTH];
   char *file_text, *c;
   long file_length, index, i, ilen;
   int  is_newline;
 
-
-  inpFile = fopen(filename, "r"); 
+  inpFile = fopen(filename, "r");
   if (inpFile == NULL) {
     printf("Error opening %s: %s\n", filename, strerror(errno));
     return -2;
   }
 
-
-
-
-  fseek(inpFile, 0L, SEEK_END); 
-  file_length = ftell(inpFile);   
+  fseek(inpFile, 0L, SEEK_END);
+  file_length = ftell(inpFile);
   rewind(inpFile);
 
   file_text = calloc(file_length + 1, sizeof(char));
@@ -62,28 +58,28 @@ int readlines(char *filename, char **textlines) {
   fread(file_text, file_length, 1, inpFile);
   fclose(inpFile);
 
-  ilen = 0;
-  
+  ilen = -1;
+
   c = file_text;
   while (*c) {
     index = 0;
     is_newline = 0;
     while (*c) {
       if (!is_newline) {
-	if (*c == CR || *c == LF) { 
+	if (*c == CR || *c == LF) {
 	  is_newline = 1;
 	}
-      } else if (*c != CR  && *c != LF) { 
+      } else if (*c != CR  && *c != LF) {
 	break;
       }
       thisline[index++] = *c++;
     }
     thisline[index] = '\0';
-    textlines[ilen] = calloc(index+1, sizeof(char)); 
+    ++ilen;
+    textlines[ilen] = calloc(index+1, sizeof(char));
     strcpy(textlines[ilen], thisline);
-    ilen++; 
-    if (ilen >= MAX_LINES) {
-      printf("\nfile has too many lines.  Limit is %ld \n " , MAX_LINES);
+    if (ilen >= MAX_LINES-1) {
+      printf("\nfile has too many lines.  Limit is %d \n " , MAX_LINES);
       return -2;
     }
   }
@@ -92,6 +88,33 @@ int readlines(char *filename, char **textlines) {
 
 }
 /*-------------------------------------------------------*/
+int make_words(char *input, char *out[], int maxwords) {
+  char *p = input;
+  int  i, nwords;
+  nwords = 0;
+  for (i = 0; i < maxwords; i++) {
+    /* skip leading whitespace */
+    while (isspace(*p)) {
+      p++;
+    }
+    if (*p != '\0') {
+      out[nwords++] = p;
+    } else {
+      out[nwords] = 0;
+      break;
+    }
+    while (*p != '\0' && !isspace(*p)) {
+      p++;
+    }
+    /* terminate arg: */
+    if (*p != '\0' && i < maxwords-1){
+      *p++ = '\0';
+    }
+  }
+  return nwords;
+}
+
+
 
 /*-------------------------------------------------------*/
 int main(int argc, char **argv) {
@@ -100,29 +123,28 @@ int main(int argc, char **argv) {
   char *header[MAX_LINES];
   char *words[MAX_WORDS];
 
-  FILE *inpFile;  
-  long  file_length, ilen, index, i;
+  FILE *inpFile;
+  long  file_length, ilen, index, i, j;
   long  ncol, nrows, nheader, nwords;
   int   is_newline;
-
   double **array;
-    
+
   /* require 2 arguments! */
   if (argc < 2) {
     show_syntax();
     return 1;
   }
-  
+
   /* read file to text lines */
   ilen = readlines(argv[1], textlines);
   if (ilen < 0) {
     return 1;
   }
-  
+
   printf("#== read %ld lines from %s\n", ilen, argv[1]);
   nheader=0;
   for (i = 0; i < ilen ; i++) {
-    if (strncmp(textlines[i], "#", 1) == 0)  { 
+    if (strncmp(textlines[i], "#", 1) == 0)  {
 	printf("  :: %s",  textlines[i]);
 	nheader++;
     } else {
@@ -133,58 +155,30 @@ int main(int argc, char **argv) {
   printf(" %ld header lines  / %ld data lines\n", nheader, ncol);
   printf(" Last header: %s", textlines[nheader-1]);
   printf(" First data: %s", textlines[nheader]);
-  
+
   nrows = make_words(textlines[nheader], words, MAX_WORDS);
-
   array = (double **)calloc(nrows, sizeof(double*));
-  for(i = 0; i < nrows; i++) { 
-    array[i] = (double*)calloc(ncol, sizeof(double)); 
+  for (i = 0; i < nrows; i++) {
+    array[i] = (double*)calloc(ncol, sizeof(double));
+    array[i][0] = strtod(words[i], NULL);
   }
 
-  
-
-  printf(" allocated %ld x %ld array elements \n", nrows, ncol);
-
-
-
-  return 0;
-} 
-/*-------------------------------------------------------*/
-
-
-int make_words(char *input, char *out[], int maxwords) {
-  char *p = input;
-  int  i, nwords;
-  nwords = 0;
-  for(i = 0; i < maxwords; i++) {
-    /* skip leading whitespace */
-    while(isspace(*p))
-      p++;
-    
-    if(*p != '\0')
-      out[nwords++] = p;
-    else {
-      out[nwords] = 0;
-      break;
+  for (i = 1; i < ncol; i++ ) {
+    nrows = make_words(textlines[nheader+i], words, MAX_WORDS);
+    for (j = 0; j < nrows; j++) {
+      array[j][i] = strtod(words[j], NULL);
     }
-    while(*p != '\0' && !isspace(*p))
-      p++;
-    /* terminate arg: */
-    if(*p != '\0' && i < maxwords-1)
-      *p++ = '\0';
   }
-  return nwords;
+  for (j = 0; j < nrows ; j++ ) {
+    printf(" J=%ld :", j);
+    for (i = 0; i < 5; i++) {
+	 printf(" %f,", array[j][i]);
+    }
+    printf("... \n");
+}
+  return 0;
 }
 
-
-
-/*  return nrow*ncol;
-
-  printf(" ...read to tear down\n");
-*/
-/* free((double *)array); */
-/*i = destroy_array(20, **array);*/
-  
 
 int build_array(int nrow, int ncol, double **array) {
   int i;
@@ -193,12 +187,12 @@ int build_array(int nrow, int ncol, double **array) {
     array[i] = (double*)calloc(ncol, sizeof(double));
   }
   return nrow*ncol;
-}  
+}
 
-int destroy_array(int nrows, double **array) { 
+int destroy_array(int nrows, double **array) {
   int i;
   for(i = 0; i < nrows; i++) {
-    printf(" free i %ld \n ", i);
+    printf(" free i %d \n ", i);
     free(array[i]);
   }
 
